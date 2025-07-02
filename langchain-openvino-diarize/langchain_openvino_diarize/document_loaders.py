63f08199e2d887c9e47991fd3e57a88ce97b69d7
gabriel-intel <g@none.com>
2025-07-02 10:20:22 -0700

    add dirization mockup

--- langchain-openvino-diarize/langchain_openvino_diarize/document_loaders.py
+++ langchain-openvino-diarize/langchain_openvino_diarize/document_loaders.py
@@ -11,6 +11,7 @@ import sys
 import torch
 import numpy as np
 
+
 class OpenVINOSpeechDiarizeLoader(BaseLoader):
     """
     OpenVINOSpeechDiarizeLoader document loader integration
@@ -68,8 +69,6 @@ class OpenVINOSpeechDiarizeLoader(BaseLoader):
             model_id: Name of the model
             device: Hardware acclerator to utilize for inference
             batch_size: Size of the batch
-            hf_token_access_token: secret
-            api_base: endpoint api
         """ # noqa: E501
         if hf_token_access_token is None:
             print("export HF_ACCESS_TOKEN=<YOUR_ACCESS_TOKEN> is necessary to download the model from HuggingFace.")
@@ -89,9 +88,12 @@ class OpenVINOSpeechDiarizeLoader(BaseLoader):
 
         from pathlib import Path
 
-        check_device = device.lower()
-        if "gpu" != check_device and "cpu" != check_device:
+        device = device.lower()
+        if "gpu" != device and "cpu" != device:
             raise NotImplementedError(f"{device} not supported")
+
+        if "gpu" == device:
+            device = "xpu"
 
         if not Path(file_path).exists():
             raise NotImplementedError(f"{file_path} does not exist")
@@ -101,72 +103,11 @@ class OpenVINOSpeechDiarizeLoader(BaseLoader):
         except ImportError as exc:
             raise ImportError(
                 "Could not import pyannote.audio python package. "
-                "Please install it with `pip`"
             ) from exc
 
-        try:
-            import openvino as ov
-        except ImportError as exc:
-            raise ImportError(
-                "Could not import openvino python package. "
-                "Please install it with pip install openvino"
-            ) from exc
-
-        from pathlib import Path
         pipeline = Pipeline.from_pretrained(self.model_id, use_auth_token=hf_token_access_token)
-
-        #core = ov.Core()
-        #ov_speaker_segmentation_path = Path("pyannote-segmentation.xml")
-
-        #if not ov_speaker_segmentation_path.exists():
-            #onnx_path = ov_speaker_segmentation_path.with_suffix(".onnx")
-        #    print("-------->Batch Size for export:", self.batch_size)
-            #torch.onnx.export(
-            #    pipeline._segmentation.model, 
-            #    torch.zeros((1, 1, 160000)), 
-            #    onnx_path, 
-            #    input_names=["chunks"], 
-            #    output_names=["outputs"], 
-            #    opset_version = 11,
-            #    do_constant_folding=False,
-#                dynamic_axes={"chunks": {0: "batch_size", self.batch_size: "wave_len"}}
-            #)
-            # remove dynamic rank by specifying input shape
-#            ov_speaker_segmentation = ov.convert_model(onnx_path, input=[1,1,160000])
-            #ov_speaker_segmentation = ov.convert_model(onnx_path, example_input=torch.rand(1,1,160000), input=[1,1,160000])
-#            ov_speaker_segmentation = ov.convert_model(pipeline._segmentation.model)
-            #ov.save_model(ov_speaker_segmentation, str(ov_speaker_segmentation_path))
-        #    print(f"Diarization model successfully cached to {ov_speaker_segmentation_path}")
-        #else:
-        #    ov_speaker_segmentation = core.read_model(ov_speaker_segmentation_path)
-        #    print(f"Diarization model successfully loaded from {ov_speaker_segmentation_path}")
-
-        #pipeline._segmentation.model = pipeline._segmentation.model.to_torchscript(method="trace")
-        #pipeline._segmentation.model = torch.compile(pipeline._segmentation.model, backend='openvino', options={"device" : "CPU" , "config" : {"PERFORMANCE_HINT" : "LATENCY"}}) # core.compile_model(ov_speaker_segmentation, self.device)
-        #pipeline._segmentation.model = ov.convert_model(pipeline._segmentation.model, example_input=torch.rand(1,1,160000))
-#        infer_request = self.ov_seg_model.create_infer_request()
-#        self.ov_seg_out = self.ov_seg_model.output(0)
-
+        pipeline.to(torch.device(device))
         self.pipeline = pipeline
-        #self.pipeline._segmentation.infer = self.infer_segm
-
-    def infer_segm(self, chunks: torch.Tensor) -> Union[np.ndarray, Tuple[np.ndarray]]:
-        """
-        Inference speaker segmentation mode using OpenVINO
-        Parameters:
-            chunks (torch.Tensor) input audio chunks
-        Return:
-            segments (np.ndarray)
-        """
-
-        res = self.ov_seg_model(chunks)
-
-        ret = res[self.ov_seg_out]        
-        #print("Sending ret: ", ret.shape)
-        #print("ret: ", ret)
-        #print("res: ", res)
-        #print("res shape: ", res.shape)
-        return ret
 
     def post_request(self, input_data: str):
         request = { 'file': open(input_data, 'rb')}
@@ -198,15 +139,13 @@ class OpenVINOSpeechDiarizeLoader(BaseLoader):
         else:
             raise NotImplementedError("Audio file type not supported")
 
-
         AUDIO_FILE = {'uri': self.file_path.split('.')[0], 'audio': self.file_path}
-        start_time = time.time()   
+        start_time = time.time()
         diarization = self.pipeline(AUDIO_FILE)
 
         docs = []
 
         for turn, _, speaker in diarization.itertracks(yield_label=True):
-            #print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
             doc = Document(
                     page_content=f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}",
                     metadata={
@@ -219,5 +158,4 @@ class OpenVINOSpeechDiarizeLoader(BaseLoader):
             docs.append(doc)
 
         result_total_latency = time.time() - start_time
-        print("Diarization Latency: ", result_total_latency)
         return docs
